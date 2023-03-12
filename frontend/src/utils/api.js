a2fcd96fe5daa38b034819b845429cd03c6fdec3
Zhenia <zhenia1985@gmail.com>
2023-03-13 00:57:46 +0700

    feat: test

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,145 +1,144 @@
 
 const getResponse = (res) => {
-    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 }
 
 class Api {
-    constructor(address) {
+   constructor(address) {
       this._address = address;
-    }
-  
-    setToken(token) {
+   }
+
+   setToken(token) {
       this._token = token;
-    }
-  
-  
-    getAppInfo() {
+   }
+
+
+   getAppInfo() {
       return Promise.all([this.getCardList(), this.getUserInfo()]);
-    }
-  
-    getCardList() {
+   }
+
+   getCardList() {
       return fetch(`${this._address}/cards`, {
-        headers: {
-          'Authorization': `Bearer ${this._token}`,
-        },
+         headers: {
+            "Authorization": `Bearer ${this._token}`,
+         },
       })
-      .then(getResponse)
-    }
-  
-    addCard({ name, link }) {
+         .then(getResponse)
+   }
+
+   addCard({ name, link }) {
       return fetch(`${this._address}/cards`, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${this._token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name,
-          link,
-        }),
+         method: "POST",
+         headers: {
+            "Authorization": `Bearer ${this._token}`,
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+            name,
+            link,
+         }),
       })
-      .then(getResponse)
-    }
-  
-    removeCard(cardId) {
+         .then(getResponse)
+   }
+
+   removeCard(cardId) {
       return fetch(`${this._address}/cards/${cardId}`, {
-        method: 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${this._token}`,
-          'Content-Type': 'application/json',
-        },
+         method: "DELETE",
+         headers: {
+            "Authorization": `Bearer ${this._token}`,
+            "Content-Type": "application/json",
+         },
       }).then(getResponse)
-    }
-  
-    getUserInfo() {
+   }
+
+   getUserInfo() {
       return fetch(`${this._address}/users/me`, {
-        headers: {
-          'Authorization': `Bearer ${this._token}`,
-          'Content-Type': 'application/json',
-        },
+         headers: {
+            "Authorization": `Bearer ${this._token}`,
+            "Content-Type": "application/json",
+         },
       })
-      .then(getResponse)
-    }
-  
-    setUserInfo({ name, about }) {
+         .then(getResponse)
+   }
+
+   setUserInfo({ name, about }) {
       return fetch(`${this._address}/users/me`, {
-        method: 'PATCH',
-        headers: {
-          'Authorization': `Bearer ${this._token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name,
-          about,
-        }),
+         method: "PATCH",
+         headers: {
+            "Authorization": `Bearer ${this._token}`,
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+            name,
+            about,
+         }),
       })
-        .then(getResponse);
-    }
-  
-    setUserAvatar({ avatar }) {
+         .then(getResponse);
+   }
+
+   setUserAvatar({ avatar }) {
       return fetch(`${this._address}/users/me/avatar`, {
-        method: 'PATCH',
-        headers: {
-          'Authorization': `Bearer ${this._token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          avatar,
-        }),
+         method: "PATCH",
+         headers: {
+            "Authorization": `Bearer ${this._token}`,
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+            avatar,
+         }),
       }).then(getResponse)
-    }
-  
-    changeLikeCardStatus(cardId, like) {
-      
+   }
+
+   changeLikeCardStatus(cardId, like) {
+
       return fetch(`${this._address}/cards/${cardId}/likes`, {
-        method: like ? 'PUT' : 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${this._token}`,
-          'Content-Type': 'application/json',
-        },
+         method: like ? "PUT" : "DELETE",
+         headers: {
+            "Authorization": `Bearer ${this._token}`,
+            "Content-Type": "application/json",
+         },
       }).then(getResponse)
-    }
-  
-    register(email, password) {
+   }
+
+   register(email, password) {
       return fetch(`${this._address}/signup`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email, password})
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({ email, password })
       })
-      .then(getResponse)
-    }
-  
-    login(email, password) {
+         .then(getResponse)
+   }
+
+   login(email, password) {
       return fetch(`${this._address}/signin`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email, password})
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({ email, password })
       })
-      .then(getResponse)
-      .then((data) => {
-        this.setToken(data.token);
-        localStorage.setItem('jwt', data.token)
-        return data;
-      })
-    }
-  
-    checkToken(token) {
+         .then(getResponse)
+         .then((data) => {
+            this.setToken(data.token);
+            localStorage.setItem("jwt", data.token)
+            return data;
+         })
+   }
+
+   checkToken(token) {
       return fetch(`${this._address}/users/me`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        }
+         method: "GET",
+         headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+         }
       })
-      .then(getResponse)
-  }
-  }
-  // Замените на адрес вашего бэкенда
-  const api = new Api('http://localhost:3000');
-  
-  export default api;
-  
+         .then(getResponse)
+   }
+}
+// Замените на адрес вашего бэкенда
+const api = new Api("https://api.http://zheniaeu.mesto.nomoredomains.work");
+
+export default api;
